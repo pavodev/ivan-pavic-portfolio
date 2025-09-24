2c599341ec8546d7efc2a7bc97c09ef23afc515a
Ivan Pavic <ivan.pavic@usi.ch>
2025-09-24 23:03:20 +0200

    PORTFOLIO-4: Added initial projects section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
 import Footer from "./components/Footer";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero />
         <Experience />
         <Education />
+        <Projects />
       </main>
       <Footer />
     </div>
